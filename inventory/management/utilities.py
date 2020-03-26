876d4f74ca5c1d933f201f1640787f79e726c87c
satshree <satshree.shrestha@yahoo.com>
2020-03-26 10:10:08 +0545

    updated log error

--- inventory/management/utilities.py
+++ inventory/management/utilities.py
@@ -6,11 +6,13 @@ def log(*args):
     """ Log Error. """
 
     file_path = os.path.join(settings.BASE_DIR, 'exception_log')
-    file_name = os.path.join(file_path, "Exception {}.txt".format(datetime.now().strftime("%Y %b. %d %I:%H:%S %p")))
+    file_name = os.path.join(file_path, "Exception {}.txt".format(datetime.now().strftime("%Y %b. %d")))
 
     with open(file_name, "a") as file:
         file.write("")
         file.write("-" * 100)
+        file.write("")
+        file.write(datetime.now().strftime("%I:%H:%S %p"))
         file.write("")
         for log in args:
             line = log + " \n"
